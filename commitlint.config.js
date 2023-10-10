98f61a3ad3e35e574b5855ccd96dee511aaf5a15
Ivan Fedotovsky <fedotovsky96@gmail.com>
2023-10-10 11:11:52 +0400

    feat: test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  messages: {
+    'type-enum': 'Неверный тип коммита. Допустимые типы: feat, fix, docs, ...',
+    'type-empty': 'Тип коммита не может быть пустым',
+  },
+};
