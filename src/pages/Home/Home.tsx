17becdbe63a3e88379a52aae547ccb7a99d86ffa
Ekaterina Kornishina <121668605+aniretakey@users.noreply.github.com>
2023-10-17 11:22:27 +0300

    feat: add modal window (#19)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,23 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { Input } from '../../components/Input';
 import Button from '../../components/Button/Button';
+import Modal from '../../components/Modal/Modal';
 
 export default function Home(): ReactElement {
+  const [modal, setModal] = useState<boolean>(false);
   return (
     <div>
       <Input />
       <Button buttonText="Button" handleClick={(): void => console.log('click')} />
+      <Button handleClick={(): void => setModal(true)} buttonText="Open modal" />
+      {modal &&
+        createPortal(
+          <Modal onClose={() => setModal(false)}>
+            <h1>Modal window</h1>
+          </Modal>,
+          document.body,
+        )}
     </div>
   );
 }
