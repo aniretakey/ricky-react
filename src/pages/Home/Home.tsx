a44ebf2140d34670cf1593469a1330d937909c83
aniretakey <EMKornishina@gmail.com>
2023-10-24 10:12:15 +0300

    feat: add cards on main page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,30 @@
 import { ReactElement } from 'react';
 import { Search } from '../../components/Search';
 import { Welcome } from '../../components/Weclome';
+import { useGetAllCharactersQuery } from '../../store/characters/characters.api.ts';
+import { CardSmall } from '../../components/CardSmall';
+import styles from './home.module.css';
 
 export function Home(): ReactElement {
+  const { data } = useGetAllCharactersQuery(null);
   return (
     <div>
       <Search />
       <Welcome />
+      <div className={styles.cards}>
+        {data?.results.map((item) => (
+          <CardSmall
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            status={item.status}
+            species={item.species}
+            type={item.type}
+            gender={item.gender}
+            image={item.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
