5fde0b226d2694cc172dcb539513601e794c9bb2
aniretakey <EMKornishina@gmail.com>
2023-10-24 14:08:02 +0300

    feat: add logout function

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,10 +2,12 @@ import { ReactElement } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../Button';
 import { useAppSelector } from '../../hooks/useAppSelector.ts';
+import { useActions } from '../../hooks/useActions.ts';
 
 export function Navigation(): ReactElement {
   const navigate = useNavigate();
   const currentUser = useAppSelector((state) => state.auth.currentUser);
+  const { setCurrentUser } = useActions();
 
   const handleSignUp = (): void => {
     navigate('/signup');
@@ -13,12 +15,26 @@ export function Navigation(): ReactElement {
   const handleLogin = (): void => {
     navigate('/login');
   };
+
+  const handleLogout = (): void => {
+    setCurrentUser('');
+    navigate('/');
+  };
+
   return (
     <nav>
-      <Button buttonText="Favourite" handleClick={(): void => console.log('click')} />
-      <Button buttonText="History" handleClick={(): void => navigate('/history')} />
-      <Button buttonText="Log in" handleClick={handleLogin} />
-      <Button buttonText="Sign up" handleClick={handleSignUp} />
+      {currentUser.login ? (
+        <>
+          <Button buttonText="Favourites" handleClick={(): void => console.log('click')} />
+          <Button buttonText="History" handleClick={(): void => navigate('/history')} />
+          <Button buttonText="Log out" handleClick={handleLogout} />
+        </>
+      ) : (
+        <>
+          <Button buttonText="Log in" handleClick={handleLogin} />
+          <Button buttonText="Sign up" handleClick={handleSignUp} />
+        </>
+      )}
     </nav>
   );
 }
