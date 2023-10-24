d74f08943d794ffc98e8abd31766a62f602cf127
aniretakey <EMKornishina@gmail.com>
2023-10-24 13:21:23 +0300

    feat: get users from local storage

--- src/store/auth/auth.slice.ts
+++ src/store/auth/auth.slice.ts
@@ -1,25 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { UserType } from "./auth.model.ts";
+import { createSlice } from '@reduxjs/toolkit';
+import { UserType } from './auth.model.ts';
 
-const initialState = {
-	users: [] as UserType[]
+const savedUsers = localStorage.getItem('users');
+let parsedUsers: UserType[] = [];
+if (savedUsers) {
+  try {
+    parsedUsers = JSON.parse(savedUsers);
+  } catch (error) {
+    console.error('Ошибка при разборе данных из localStorage', error);
+  }
 }
 
+const initialState = {
+  users: parsedUsers,
+};
+
 export const authSlice = createSlice({
-	name: 'auth',
-	initialState,
-	reducers: {
-		confirm(state, action){
-			console.log('confirm')
-			console.log(state)
-			console.log(action)
-		},
-		addUser(state, { payload}){
-			state.users.push(payload)
-			localStorage.setItem('users', JSON.stringify(state.users));
-		},
+  name: 'auth',
+  initialState,
+  reducers: {
+    confirm(state, action) {
+      console.log('confirm');
+      console.log(state);
+      console.log(action);
+    },
+    addUser(state, { payload }) {
+      state.users.push(payload);
+      localStorage.setItem('users', JSON.stringify(state.users));
+    },
+  },
+});
 
-	}
-})
-
-export const { actions, reducer } = authSlice
+export const { actions, reducer } = authSlice;
